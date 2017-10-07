c13ad433821235f1e65e20eefe7e4bf7250bb27f
Zachary Cooper <zackjcooper@gmail.com>
2017-10-07 17:34:42 -0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  devServer: {
+    contentBase: path.resolve(__dirname, './src')
+  },
+  entry: ['./app.js', './scss/base.scss'],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/ajv/),
+    new ExtractTextPlugin({
+      filename: '[name].bundle.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'head',
+    }),
+    new CopyWebpackPlugin(
+      [{ from: 'images', to: 'images' }]
+    )
+  ],
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    console: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['env'] },
+        }],
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+};
