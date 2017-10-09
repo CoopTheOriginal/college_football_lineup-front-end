915432919457db878e9ae4b4894e39de5008b8be
Zachary Cooper <zackjcooper@gmail.com>
2017-10-08 22:29:42 -0400

    added pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,43 @@
 import $ from 'jquery';
 
+$(document).ready(() => {
+  $('#IncludeHeader').load('header.html');
+  $('#IncludeSignUp').load('sign_up.html');
+  $('#IncludeHowToPlay').load('how_to_play.html');
+  $('#IncludeFooter').load('footer.html');
 
-$(document).ready(function () {
-  console.log('hi');
+  $('body').on('click', '#goHome', () => {
+    $('#mainBody').empty()
+      .append($('<div>').load('how_to_play.html'))
+      .append($('<div>').load('sign_up.html'));
+  });
+
+  $('body').on('click', '#leagues', () => {
+    $('#mainBody').empty().load('leagues.html');
+  });
+
+  $('body').on('click', '#lineUp', () => {
+    $('#mainBody').empty().load('line_up.html');
+    console.log('lineUp');
+  });
+
+  $('body').on('click', '#signUp', () => {
+    $('#mainBody').empty().load('sign_up.html');
+  });
+
+  $('body').on('click', '#aboutUs', () => {
+    $('#mainBody').empty();
+    console.log('aboutUs');
+  });
+
+  $('body').on('click', '#support', () => {
+    $('#mainBody').empty();
+    console.log('support');
+  });
+
+  $('body').on('click', '#logIn', () => {
+    $('#mainBody').empty().load('log_in.html');
+  });
 });
+
+
